cc2890b9a901b12d28dc1c2bacdc175cf70a2d09
JohnMalugu <johnmalugu99@gmail.com>
2026-02-04 22:27:29 +0300

    Initial commit: Spring Boot reporting structure

---
+++ src/test/java/com/jcmlabs/spring_report/SpringReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcmlabs.spring_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
