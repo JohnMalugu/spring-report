0ba2995d9d4c10c84f51f26bdf6d942e99d81c0e
JohnMalugu <johnmalugu99@gmail.com>
2026-02-06 18:19:46 +0300

    jpa repository employee

--- src/main/java/com/jcmlabs/spring_report/repositories/EmployeeRepository.java
+++ src/main/java/com/jcmlabs/spring_report/repositories/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.jcmlabs.spring_report.repositories;
 
 import com.jcmlabs.spring_report.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Long, Employee> {
 }
