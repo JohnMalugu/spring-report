7bad1e7ae8098fb2a8e1ea09287ac6e287e25656
JohnMalugu <johnmalugu99@gmail.com>
2026-02-06 18:21:39 +0300

    report type enum

--- src/main/java/com/jcmlabs/spring_report/models/Employee.java
+++ src/main/java/com/jcmlabs/spring_report/models/Employee.java
@@ -1,5 +1,10 @@
 package com.jcmlabs.spring_report.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 @Entity
 public class Employee {
     @Id
