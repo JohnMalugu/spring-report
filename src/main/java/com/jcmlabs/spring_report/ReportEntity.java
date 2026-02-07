29763fe9c3195a896298b38ec8604b7e702950f1
JohnMalugu <johnmalugu99@gmail.com>
2026-02-07 21:09:09 +0300

    getters and setters

--- src/main/java/com/jcmlabs/spring_report/ReportEntity.java
+++ src/main/java/com/jcmlabs/spring_report/ReportEntity.java
@@ -1,5 +1,14 @@
 package com.jcmlabs.spring_report;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "report")
+@Getter
+@Setter
 public class ReportEntity {
     private String test;
 }
