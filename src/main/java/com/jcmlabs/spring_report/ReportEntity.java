181a875880b2d292e7fc53aa4c532dfb0eb7c895
JohnMalugu <johnmalugu99@gmail.com>
2026-02-07 21:10:04 +0300

    constructors and soft delete

--- src/main/java/com/jcmlabs/spring_report/ReportEntity.java
+++ src/main/java/com/jcmlabs/spring_report/ReportEntity.java
@@ -2,13 +2,19 @@ package com.jcmlabs.spring_report;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SoftDelete;
 
 @Entity
 @Table(name = "report")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SoftDelete
 public class ReportEntity {
     private String test;
 }
