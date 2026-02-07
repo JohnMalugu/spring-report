b67a2ab495d17e49fcc49a985005a680a36fe8b5
JohnMalugu <johnmalugu99@gmail.com>
2026-02-07 22:17:25 +0300

    report util

--- src/main/java/com/jcmlabs/spring_report/utils/JasperReportsUtil.java
+++ src/main/java/com/jcmlabs/spring_report/utils/JasperReportsUtil.java
@@ -1,4 +1,27 @@
 package com.jcmlabs.spring_report.utils;
 
+@Component
 public class JasperReportsUtil {
+
+    public byte[] exportJasperReportBytes(JasperPrint jasperPrint, ReportTypeEnum reportType) throws JRException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        switch (reportType) {
+            case XLSX -> {
+                JRXlsxExporter exporter = new JRXlsxExporter();
+                exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+                exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outputStream));
+                exporter.exportReport();
+            }
+            case DOC -> {
+                JRRtfExporter exporter = new JRRtfExporter();
+                exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+                exporter.setExporterOutput(new SimpleWriterExporterOutput(outputStream));
+                exporter.exportReport();
+            }
+            case PDF -> JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
+            default -> throw new IllegalArgumentException("Unsupported format");
+        }
+        return outputStream.toByteArray();
+    }
 }
+
