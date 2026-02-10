a3f9184168641bba44315b999f0f7da0f9f07140
JohnMalugu <johnmalugu99@gmail.com>
2026-02-10 21:48:02 +0300

    extending base entity

--- src/main/java/com/jcmlabs/spring_report/shared/entities/BaseEntity.java
+++ src/main/java/com/jcmlabs/spring_report/shared/entities/BaseEntity.java
@@ -3,7 +3,10 @@ package com.jcmlabs.spring_report.shared.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.MappedSuperclass;
 
+import java.time.LocalDateTime;
+
 @MappedSuperclass
 @Entity
 public class BaseEntity {
+    private LocalDateTime createdAt;
 }
