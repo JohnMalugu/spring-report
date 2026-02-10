eaee38230163fbd8bb36b373904d7261d24ce025
JohnMalugu <johnmalugu99@gmail.com>
2026-02-10 21:59:50 +0300

    uuid

--- src/main/java/com/jcmlabs/spring_report/shared/entities/BaseEntity.java
+++ src/main/java/com/jcmlabs/spring_report/shared/entities/BaseEntity.java
@@ -3,6 +3,7 @@ package com.jcmlabs.spring_report.shared.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @MappedSuperclass
 @Entity
@@ -12,12 +13,13 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
+    private String uuid = UUID.randomUUID().toString();
+
     private LocalDateTime createdAt;
     private Long createdBy;
 
     private LocalDateTime updatedAt;
     private Long updatedBy;
-
-    private String uuid;
 
 }
