76395695e4f19babe560075dbbf3eb105a6899c7
JohnMalugu <johnmalugu99@gmail.com>
2026-02-11 21:41:35 +0300

    feb 11

--- src/main/java/com/jcmlabs/spring_report/models/Employee.java
+++ src/main/java/com/jcmlabs/spring_report/models/Employee.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.SoftDelete;
 
 @Getter
