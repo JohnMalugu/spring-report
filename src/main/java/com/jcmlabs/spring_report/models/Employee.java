f1ff35d79a3aea3d5352d04067f933874a1432f0
JohnMalugu <johnmalugu99@gmail.com>
2026-02-13 00:55:21 +0300

    xx

--- src/main/java/com/jcmlabs/spring_report/models/Employee.java
+++ src/main/java/com/jcmlabs/spring_report/models/Employee.java
@@ -1,5 +1,7 @@
 package com.jcmlabs.spring_report.models;
 
+import com.jcmlabs.spring_report.shared.entities.BaseEntity;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
