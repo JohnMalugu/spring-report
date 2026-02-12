b3c35b6743d7e690cbbc5c8e5d3f5c9b46bbaa8e
JohnMalugu <johnmalugu99@gmail.com>
2026-02-13 00:30:30 +0300

    xx

--- src/main/java/com/jcmlabs/spring_report/shared/entities/BaseEntity.java
+++ src/main/java/com/jcmlabs/spring_report/shared/entities/BaseEntity.java
@@ -1,9 +1,7 @@
 package com.jcmlabs.spring_report.shared.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
