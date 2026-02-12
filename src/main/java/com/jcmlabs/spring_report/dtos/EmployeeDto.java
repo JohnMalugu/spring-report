d6ecabefd0fcc60f9328e967367053675d7eafd7
JohnMalugu <johnmalugu99@gmail.com>
2026-02-12 23:29:54 +0300

    Employee dto validation

--- src/main/java/com/jcmlabs/spring_report/dtos/EmployeeDto.java
+++ src/main/java/com/jcmlabs/spring_report/dtos/EmployeeDto.java
@@ -2,6 +2,9 @@ package com.jcmlabs.spring_report.dtos;
 
 public record EmployeeDto(
         Long id,
+
+        @NotBlank(message = "Name is required")
+        @Size(max = 255)
         String name,
         String city,
         Double salary) {}
