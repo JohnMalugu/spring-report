546ab72cd4d52cc18e845b13a977e7555bfb95a2
JohnMalugu <johnmalugu99@gmail.com>
2026-02-13 00:13:48 +0300

    salary shud be greater than zero and notnull

--- src/main/java/com/jcmlabs/spring_report/dtos/EmployeeDto.java
+++ src/main/java/com/jcmlabs/spring_report/dtos/EmployeeDto.java
@@ -1,6 +1,8 @@
 package com.jcmlabs.spring_report.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record EmployeeDto(
@@ -12,4 +14,6 @@ public record EmployeeDto(
         @Size(max = 50)
         String city,
 
+        @NotNull(message = "Salary is required")
+        @Positive(message = "Salary must be greater than zero")
         Double salary) {}
