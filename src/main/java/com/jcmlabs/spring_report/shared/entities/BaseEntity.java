30a8b6b6d43c5e9170dfd0d8480475ca3dc5df4c
JohnMalugu <johnmalugu99@gmail.com>
2026-02-14 18:23:36 +0300

    made base entity abstract class

--- src/main/java/com/jcmlabs/spring_report/shared/entities/BaseEntity.java
+++ src/main/java/com/jcmlabs/spring_report/shared/entities/BaseEntity.java
@@ -24,7 +24,11 @@ public abstract class BaseEntity {
 
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @Column(name = "created_by")
     private Long createdBy;
+
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
     private Long updatedBy;
 }
