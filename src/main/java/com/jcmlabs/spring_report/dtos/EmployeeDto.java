d0c655ca555ed877059eb1f9f5813e4c4f272074
JohnMalugu <johnmalugu99@gmail.com>
2026-02-16 18:43:31 +0300

    code re formatted

--- src/main/java/com/jcmlabs/spring_report/dtos/EmployeeDto.java
+++ src/main/java/com/jcmlabs/spring_report/dtos/EmployeeDto.java
@@ -6,12 +6,16 @@ import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record EmployeeDto(
+        Long id,
+        String uuid,
 
-        Long id, String uuid,
-
-        @NotBlank(message = "Name is required") @Size(min = 2, max = 255, message = "Name must be between 2 and 255 characters") String name,
+        @NotBlank(message = "Name is required")
+        @Size(min = 2, max = 255, message = "Name must be between 2 and 255 characters")
+        String name,
 
         @Size(max = 50) String city,
 
-        @NotNull(message = "Salary is required") @Positive(message = "Salary must be greater than zero") Double salary) {
-}
+        @NotNull(message = "Salary is required")
+        @Positive(message = "Salary must be greater than zero")
+        Double salary)
+{}
