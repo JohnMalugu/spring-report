57c5278c47a7c136c35e662e3b6d50ab9308ed3a
JohnMalugu <johnmalugu99@gmail.com>
2026-02-19 23:43:17 +0300

    removed local date time from created at abd using instant

--- src/main/java/com/jcmlabs/spring_report/ReportEntity.java
+++ src/main/java/com/jcmlabs/spring_report/ReportEntity.java
@@ -2,6 +2,7 @@ package com.jcmlabs.spring_report;
 
 import com.jcmlabs.spring_report.shared.entities.BaseEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -20,6 +21,7 @@ import org.hibernate.annotations.SQLRestriction;
 @SQLDelete(sql = "UPDATE report SET deleted = true WHERE id = ?")
 @SQLRestriction("deleted = false")
 public class ReportEntity extends BaseEntity {
+    @Column
     private String test;
     private String description;
 }
