5c790020c935e6e9cb67bbfac85df7a5440c5408
JohnMalugu <johnmalugu99@gmail.com>
2026-02-21 21:13:27 +0300

    report dto

--- src/main/java/com/jcmlabs/spring_report/dtos/ReportDto.java
+++ src/main/java/com/jcmlabs/spring_report/dtos/ReportDto.java
@@ -1,4 +1,7 @@
 package com.jcmlabs.spring_report.dtos;
 
-public record ReportDto(String name, String description, String owner) {
+public record ReportDto(
+        String name,
+        String description,
+        String owner) {
 }
