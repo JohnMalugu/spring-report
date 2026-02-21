7103d64ec38ff9a556b148d2755a95fa781e3bbe
JohnMalugu <johnmalugu99@gmail.com>
2026-02-21 21:13:44 +0300

    report dto

--- src/main/java/com/jcmlabs/spring_report/dtos/ReportDto.java
+++ src/main/java/com/jcmlabs/spring_report/dtos/ReportDto.java
@@ -1,6 +1,9 @@
 package com.jcmlabs.spring_report.dtos;
 
+import jakarta.validation.constraints.NotNull;
+
 public record ReportDto(
+        @NotNull
         String name,
         String description,
         String owner) {
