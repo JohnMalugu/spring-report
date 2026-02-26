e10cd62ae9a67b2e58013c3431d5eb7366d388f6
JohnMalugu <johnmalugu99@gmail.com>
2026-02-26 19:09:18 +0300

    minimum name size

--- src/main/java/com/jcmlabs/spring_report/dtos/ReportDto.java
+++ src/main/java/com/jcmlabs/spring_report/dtos/ReportDto.java
@@ -1,9 +1,11 @@
 package com.jcmlabs.spring_report.dtos;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record ReportDto(
         @NotNull(message = "Name is required")
+        @Size(min = 10)
         String name,
 
         @NotNull(message = "description is required, can't be null")
