b073718a191164be00067513d4ebd3ac8a6530bb
JohnMalugu <johnmalugu99@gmail.com>
2026-03-02 09:24:00 +0300

    argumented constructorts

--- src/main/java/com/jcmlabs/spring_report/models/UserAccount.java
+++ src/main/java/com/jcmlabs/spring_report/models/UserAccount.java
@@ -3,11 +3,14 @@ package com.jcmlabs.spring_report.models;
 import com.jcmlabs.spring_report.shared.entities.BaseEntity;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+
 public class UserAccount extends BaseEntity {
     private String firstName;
     private String middleName;
