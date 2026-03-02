f2f2871cffebd68416863f41ee51f06d09499ff4
JohnMalugu <johnmalugu99@gmail.com>
2026-03-02 09:25:16 +0300

    from user account to users

--- src/main/java/com/jcmlabs/spring_report/models/UserAccount.java
+++ src/main/java/com/jcmlabs/spring_report/models/UserAccount.java
@@ -2,6 +2,7 @@ package com.jcmlabs.spring_report.models;
 
 import com.jcmlabs.spring_report.shared.entities.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "users")
 public class UserAccount extends BaseEntity {
     private String firstName;
     private String middleName;
