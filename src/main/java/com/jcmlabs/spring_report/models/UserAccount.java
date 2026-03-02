aa6b5f0b223c85fab3e2d6f59e3987df33deac4f
JohnMalugu <johnmalugu99@gmail.com>
2026-03-02 09:20:12 +0300

    UserAccount entity,names

--- src/main/java/com/jcmlabs/spring_report/models/UserAccount.java
+++ src/main/java/com/jcmlabs/spring_report/models/UserAccount.java
@@ -2,8 +2,10 @@ package com.jcmlabs.spring_report.models;
 
 import com.jcmlabs.spring_report.shared.entities.BaseEntity;
 import jakarta.persistence.Entity;
+import lombok.Setter;
 
 @Entity
+@Setter
 public class UserAccount extends BaseEntity {
     private String firstName;
     private String middleName;
